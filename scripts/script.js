9e6c0c44506f1dcd675ac79f751ada6149f15bb3
Mila Rut <mila.balashova2312@gmail.com>
2022-10-23 00:37:52 +0300

    Рефакторинг скрипта модального окна

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,15 @@
 let modalContainer = document.querySelector('.modal-container');
-let modalToggle = document.querySelector('.hotel-search-button');
-let modalCloseButton = document.querySelector('.modal-close-button');
+let modalOpenBtn = document.querySelector('.hotel-search-button');
+let modalCloseBtn = document.querySelector('.modal-close-button');
 
-modalToggle.addEventListener('click', function () {
-    if (modalContainer.classList.contains('modal-container--closed')) {
-        modalContainer.classList.remove('modal-container--closed');
-        modalContainer.classList.add('modal-container--open');
-    }
-});
+const toggleModal = () => modalContainer.classList.toggle('modal-container--closed');
 
-modalCloseButton.addEventListener('click', function () {
-    if (modalContainer.classList.contains('modal-container--open')) {
-        modalContainer.classList.remove('modal-container--open');
-        modalContainer.classList.add('modal-container--closed');
-    }
+modalOpenBtn.addEventListener('click', toggleModal);
+modalCloseBtn.addEventListener('click', toggleModal);
+
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    modalContainer.classList.toggle('modal-container--closed');
+  }
 });
